2748864e4a439c55765d0221d22dc09c083cde9f
Lucas Conde <lucasconde742@gmail.com>
2025-06-03 14:54:32 -0300

    Restructurando index

--- index.php
+++ index.php
@@ -1,27 +1,25 @@
-<?php
-    include_once('config/conexion.php');
-?>
+<?php  include('config/conexion.php'); ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Auron Store</title>
+    <!-- META / TITLE -->
+    <?php include('layout/meta.php'); ?>
+
+    <!-- ICONOS -->
+    <?php include('layout/iconos.php'); ?>
+
+    <!-- CSS -->
     <link rel="stylesheet" href="css/index.css">
-    <script src="https://kit.fontawesome.com/439ee37b3b.js" crossorigin="anonymous"></script>
 </head>
 <body>
-
-
     <main>
-        <?php
-            include("layout/nav.php")
-        ?>
+        <!-- MENU DE NAVEGACION -->
+        <?php include("layout/nav.php"); ?>
 
+        <!-- CATEGORIAS PARA ELEGIR -->
         <div id="categories">
             <div>
-                <!-- <i class="fas fa-stream"></i> -->
                 <label for="cate">
                     <i class="fas fa-outdent fa-flip-horizontal"></i>
                 </label>
@@ -29,18 +27,21 @@
                 <select name="" id="cate" onchange="location = this.value">
                     <option value="" selected disabled="disabled">Selecione una categoria</option>
                         <?php
-                            while($fila = $rubro->fetch()){
-                                ?>
-                                    <option value="vistas/rubro.php?id=<?php echo $fila['id']; ?>"><?php echo $fila['nombre']; ?></option>
-                                <?php
-                            }
-                        ?>
-                </select>
+                            $rubro = $con->query("SELECT *
+                                                    FROM invRubro
+                                                    WHERE nombre NOT LIKE 'rubro%'
+                                                    ORDER BY nombre");
 
-                
+                            while($fila = $rubro->fetch()){ ?>
+                                    <option value="vistas/rubro.php?id=<?php echo $fila['id']; ?>">
+                                        <?php echo $fila['nombre']; ?>
+                                    </option>
+                        <?php } ?>
+                </select>
             </div>
         </div>
 
+        <!-- OFERTAS / SLIDERS AUTOMATICO -->
         <div id="offers">
             <div>
                 <section>
@@ -55,18 +56,15 @@
                     </div>
 
                     <?php
+                        $ofertas = $con->query("SELECT TOP 5 *
+                                                    FROM [Gestion].[dbo].[invItem]
+                                                    WHERE tieneFoto = 1");
                         
-
-                        while($fila = $ofertas->fetch()){
-                            ?>
-                                
-                                <a href="vistas/item.php?id=<?php echo $fila['id']; ?>">
-                                    <img  src="assets/img/<?php echo $fila['codigo']; ?>.jpg" alt="<?php echo $fila['nombre']; ?>">
-                                </a>
-                            <?php
-                        }
-                    ?>
-
+                        while($fila = $ofertas->fetch()){ ?>
+                            <a href="vistas/item.php?id=<?php echo $fila['id']; ?>">
+                                <img  src="assets/img/image.jpg" alt="<?php echo $fila['nombre']; ?>">
+                            </a>
+                    <?php } ?>
 
                     <div class="arrow arrow-right">
                         <i class="fas fa-chevron-circle-right"></i>
@@ -93,40 +91,49 @@
             </div>
         </div>
 
+        <!-- LO MAS VENDIDO -->
         <div id="articles">
             <section>
                 <h2>Lo más vendidos</h2>
             </section>
-            <div>
-                <?php
-                    while($fila = $MasVendidos->fetch()){
-                        ?>
-                            <div>
-                                <a href="vistas/item.php?id=<?php echo $fila['id']; ?>">
-                                    <img src="assets/img/<?php echo $fila['codigo']; ?>.jpg" alt="<?php echo $fila['nombre']; ?>">
-                                    <div>
-                                        <p>
-                                            <?php echo $fila['nombre']; ?>
-                                        </p>
-                                        <hr>
-                                        <p>
-                                            <?php echo $fila['C_IVA']; ?>
-                                        </p>
-                                    </div>
-                                </a>
-                            </div>
-                        <?php
-                    }
-                ?>
+            <div class="container_products">
+                <?php $MasVendidos = $con->query("SELECT invItem.id,
+                                                        LEFT (invItem.nombre, 15) AS nombre,
+                                                        invItem.codigo,
+                                                        invItem.tienefoto,
+                                                        invPrecioItem.precio,
+                                                        LEFT (invPrecioItem.precio * 1.21, 7) AS C_IVA
+                                                    FROM invItem
+                                                    RIGHT JOIN invPrecioItem
+                                                    ON invItem.id = invPrecioItem.idItem
+                                                    WHERE referenciaArea = 'VTA'
+                                                        AND invItem.tienefoto = 1
+                                                        AND idrubro = 'EE27FC88-067F-4CE9-8DE2-280C1976C9EB' ");
+
+                    while($fila = $MasVendidos->fetch()){ ?>
+                        <div class="products">
+                            <a href="vistas/item.php?id=<?php echo $fila['id']; ?>">
+                                <img src="assets/img/image.jpg" alt="<?php echo $fila['nombre']; ?>">
+                                <div>
+                                    <p>
+                                        <?php echo $fila['nombre']; ?>
+                                    </p>
+                                    <hr>
+                                    <p>
+                                        <?php echo $fila['C_IVA']; ?>
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                <?php } ?>
             </div>
             <button>
                 <a href="http://lucasconde.ddns.net/AuronStore/vistas/productos.php">ver todos</a>
             </button>
         </div>
 
-        <?php
-            include("layout/footer.php")
-        ?>
+        <!-- PIE DE PAGINA -->
+        <?php include("layout/footer.php"); ?>
     </main>
 
     <script src="js/script.js"></script>
