25d876560c80db8ac267a67ea91ccf1ad85e8dab
Lucas Conde <lucasconde742@gmail.com>
2025-06-03 15:01:25 -0300

    Agregando include iconos/meta

---
+++ layout/meta.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Este sitio en una vista de articulos del local Auron Store.">
+<meta name="keywords" content="website, phone, celular, fundas, usb, cargador, cable usb, cargador celular, cargador de celular, cargador de telefono, cargador de telefono, cargador de celular barato, cargador de telefono barato, cargador usb, cargador usb celular, cargador usb telefono, cargador usb barato, fundas celulares, fundas telefonos, fundas celulares baratas, fundas telefonos baratas">
+<meta name="robots" content="index, follow">
+<meta name="author" content="Lucas Conde">
+
+<title>Auron Store</title>
