e3ab0ae441ac634700b63d07f72e473535a378ed
Lucas Conde <lucasconde742@gmail.com>
2025-06-04 13:58:57 -0300

    Agregando seccion de categorias

---
+++ layout/categories.php
@@ -0,0 +1,23 @@
+<!-- CATEGORIAS PARA ELEGIR -->
+<div id="categories">
+    <div>
+        <label for="cate">
+            <i class="fas fa-outdent fa-flip-horizontal"></i>
+        </label>
+
+        <select name="" id="cate" onchange="location = this.value">
+            <option value="" selected disabled="disabled">Selecione una categoria</option>
+                <?php
+                    $rubro = $con->query("SELECT *
+                                            FROM invRubro
+                                            WHERE nombre NOT LIKE 'rubro%'
+                                            ORDER BY nombre");
+
+                    while($fila = $rubro->fetch()){ ?>
+                            <option value="http://lucasconde.ddns.net/AuronStore/vistas/rubros.php?id=<?php echo $fila['id']; ?>">
+                                <?php echo $fila['nombre']; ?>
+                            </option>
+                <?php } ?>
+        </select>
+    </div>
+</div>
